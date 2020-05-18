992efa73faf652162095203d47acfa2a97612661
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2020-05-18 18:57:34 +0900

    Back End Back up

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dovenv');
+//import routes
+const authRoute = require('./routes/auth');
+
+
+dotenv.config();
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log('connected to database!')
+);
+
+//Middlewears
+app.use(express.json());
+
+//route middlewears
+app.use('/api/user', authRoute);
+
+app.listen(3000, () => console.log("server up and running"));
