673e39a84c87ae1e6e1f2aaa43941c35110a11fe
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2020-07-26 08:47:47 +0900

    fixed!

--- model/Validation.js
+++ model/Validation.js
@@ -1,28 +1,60 @@
+const Joi = require("@hapi/joi");
 
-//Validation
-const Joi = require('@hapi/joi');
+// defined here so we only need to change them in ...
+// ... one file instead of changing in Models too
+const validationLengths = {
+  name: {
+    min: 6,
+    max: 100,
+  },
+  email: {
+    min: 6,
+    max: 320,
+  },
+  password: {
+    min: 6,
+    max: 30,
+  },
+};
 
-//reg-validation
+//(JSON.parse(req.body))
+
 const registerValidation = data => {
-
-const schema = {
-    name: Joi.string().min(6),required(),
-    email: Joi.string().min(6),required().email(),
-    password: JOI.string().min(6),required(),
+  const schema = Joi.object({
+    name: Joi.string()
+      .min(validationLengths.name.min)
+      .max(validationLengths.name.max)
+      .required(),
+    email: Joi.string()
+      .min(validationLengths.email.min)
+      .max(validationLengths.email.max)
+      .email()
+      .required(),
+    password: Joi.string()
+      .min(validationLengths.password.min)
+      .max(validationLengths.password.max)
+      .required(),
+  });
+  return schema.validate(data);
 };
-    return Joi.validate(data, schema);
-};
 
-//login-validation
 const loginValidation = data => {
-
-const schema = {
-    email: Joi.string().min(6),required().email(),
-    password: JOI.string().min(6),required(),
+  const schema = Joi.object({
+    email: Joi.string()
+      .min(validationLengths.email.min)
+      .max(validationLengths.email.max)
+      .email()
+      .required(),
+    password: Joi.string()
+      .min(validationLengths.password.min)
+      .max(validationLengths.password.max)
+      .required(),
+  });
+  return schema.validate(data);
 };
-    return Joi.validate(data, schema);
+
+module.exports = {
+  validationLengths,
+  registerValidation,
+  loginValidation,
 };
-
-module.exports.registerValidation = registerValidation;
-module.exports.loginValidation = loginValidation;
-
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt.js');
-const {registerValidation, loginValidation} = require('../Validation');
+const bcrypt = require('bcrypt');
+const {registerValidation, loginValidation} = require('./../model/Validation');
 
 router.post('/register', async (req, res) => {
-
+console.log("start")
 //LETS VALIDATE THE DATA BEFORE WE ADD A User
 const {error} = registerValidation(req.body);
 if (error) return res.status(400).send(error.details[0].message);
 
-//checklog if the user is already in the database
+//check log if the user is already in the database
 const emailExist = await User.findOne({email: req.body.email});
 if (emailExist) return res.status(400).send('Email already exists');
 
@@ -33,6 +33,43 @@ const hashedPassword = await bcrypt.hash(req.body.password, salt);
    }
 });
 
+// delete user
+router.delete("/delete/", async (req, res) => {
+  const user = await User.findOne({email: req.body.email});
+  const email = req.params.email
+  console.log(user);
+  try {
+    await User.deleteOne({"email": email})
+    res.status(200).send("Deleted")
+  } catch (error) {
+    res.status(400).send(error)
+  }
+});
+
+// get info
+router.get("/user/", async (req, res) => {
+  try {
+    const user = await User.findOne({email: req.body.email});
+    console.log(user)
+    res.status(200).send(user)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+});
+
+// update
+router.patch("/patch", async (req, res) => {
+  console.log("patches")
+  try {
+    const updateObject = req.body; // {last_name : "smith", age: 44}
+    const id = {email: req.body.email};
+    const user = await User.update({email  : id}, {$set: updateObject});
+    res.status(200).send(user)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+});
+
 // login
 router.post('/login', async (req, res) => {
     const {error} = loginValidation(req.body);
@@ -42,163 +79,9 @@ router.post('/login', async (req, res) => {
     const validPass = await bcrypt.compare(req.body.password, user.password);
     if(!validPass) return res.status(400).send('Email or Password is Wrong');
 
-    // reate and asign a token
+    // create and assign a token
     const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
     res.header('auth-token', token).send(token);
 });
-
-// user pays , 
-router.post('/paid', async (req, res) => {
-    (console.log('paid route'))
- });
-
-//reset password 
-// router.post('/reset', async (req, res) => {
-//     const {error} = loginValidation(req.body);
-//     if (error) return res.status(400).send(error.details[0].message);
-//     const user = await User.findOne({email: req.body.email});
-//     if (!user) return res.status(400).send('User by this email is not found')
-//     // replace password? 
-//  });
-
- // reset password 
-
- app.get('/reset/:token', function(req, res) {
-    User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
-        console.log(user);
-      if (!user) {
-        req.flash('error', 'Password reset token is invalid or has expired.');
-        return res.redirect('/forgot');
-      }
-      res.render('reset', {
-       User: req.user
-      });
-    });
-  });
-  
-  app.post('/reset/:token', function(req, res) {
-    async.waterfall([
-      function(done) {
-        User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user, next) {
-          if (!user) {
-            req.flash('error', 'Password reset token is invalid or has expired.');
-            return res.redirect('back');
-          }
-  
-  
-          user.password = req.body.password;
-          user.resetPasswordToken = undefined;
-          user.resetPasswordExpires = undefined;
-          console.log('password' + user.password  + 'and the user is' + user)
-  
-  user.save(function(err) {
-    if (err) {
-        console.log('here')
-         return res.redirect('back');
-    } else { 
-        console.log('here2')
-      req.logIn(user, function(err) {
-        done(err, user);
-      });
-  
-    }
-          });
-        });
-      },
-  
-      function(user, done) {
-          // console.log('got this far 4')
-        var smtpTrans = nodemailer.createTransport({
-          service: 'Gmail',
-          auth: {
-            user: 'myemail',
-            pass: 'mypass'
-            // put email and password into here maybe use dotenv for both
-          }
-        });
-        var mailOptions = {
-          to: user.email,
-          from: 'myemail',
-          subject: 'Your password has been changed',
-          text: 'Hello,\n\n' +
-            ' - This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
-        };
-        smtpTrans.sendMail(mailOptions, function(err) {
-          // req.flash('success', 'Success! Your password has been changed.');
-          done(err);
-        });
-      }
-    ], function(err) {
-      res.redirect('/');
-    });
-  });
-
- // forgot password
- app.post('/forgot', function(req, res, next) {
-    async.waterfall([
-      function(done) {
-        crypto.randomBytes(20, function(err, buf) {
-          var token = buf.toString('hex');
-          done(err, token);
-        });
-      },
-      function(token, done) {
-        User.findOne({ email: req.body.email }, function(err, user) {
-          if (!user) {
-          //   console.log('error', 'No account with that email address exists.');
-          req.flash('error', 'No account with that email address exists.');
-            return res.redirect('/forgot');
-          }
-  console.log('step 1')
-          user.resetPasswordToken = token;
-          user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
-  
-          user.save(function(err) {
-            done(err, token, user);
-          });
-        });
-      },
-      function(token, user, done) {
-          console.log('step 2')
-  
-  
-        var smtpTrans = nodemailer.createTransport({
-           service: 'Gmail', 
-           auth: {
-            user: 'myemail',
-            pass: 'mypassword'
-          }
-        });
-        var mailOptions = {
-  
-          to: user.email,
-          from: 'myemail',
-          subject: 'Node.js Password Reset',
-          text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-            'http://' + req.headers.host + '/reset/' + token + '\n\n' +
-            'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-  
-        };
-        console.log('step 3')
-  
-          smtpTrans.sendMail(mailOptions, function(err) {
-          req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-          console.log('sent')
-          res.redirect('/forgot');
-  });
-  }
-    ], function(err) {
-      console.log('this err' + ' ' + err)
-      res.redirect('/');
-    });
-  });
-  
-  app.get('/forgot', function(req, res) {
-    res.render('forgot', {
-      User: req.user
-    });
-  });
-  
 
 module.exports = router;
